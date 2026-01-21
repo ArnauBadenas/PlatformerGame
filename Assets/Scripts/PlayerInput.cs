90e60c5151e7625dc912e54599befc4a37a73a37
Carla Pallés Ros <carlaa.pr15@gmail.com>
2026-01-21 22:15:46 +0100

    Added idle animation

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -5,32 +5,48 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerInput : MonoBehaviour
 {
-    [SerializeField] float walkSpeed = 5.0f;
-
     Rigidbody2D rb2D;
     [SerializeField] Animator animator;
 
-    InputAction moveAction;
-    Vector2 movement;
+    float horizontalDirection;
+    [SerializeField] float speed = 5.0f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    bool facingRight = true;
+
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
-
-        moveAction = InputSystem.actions.FindAction("Move");
-        moveAction.Enable();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        movement = moveAction.ReadValue<Vector2>();
-        animator.SetFloat("Walk", Mathf.Abs(movement.x));
     }
 
     void FixedUpdate()
     {
-        rb2D.linearVelocity = new Vector2(movement.x * walkSpeed, rb2D.linearVelocityY);
+        Vector2 velocity = rb2D.linearVelocity;
+        velocity.x = horizontalDirection * speed;
+        rb2D.linearVelocity = velocity;
+        
+        animator.SetFloat("Walk", Mathf.Abs(horizontalDirection));
+
+        if (horizontalDirection > 0 && !facingRight)
+        {
+            FlipCharacter();
+        } else if (horizontalDirection < 0 && facingRight)
+        {
+            FlipCharacter();
+        }
+    }
+
+    void OnMove(InputValue value)
+    {
+        var input = value.Get<Vector2>();
+        horizontalDirection = input.x;
+    }
+
+    void FlipCharacter()
+    {
+        Vector3 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+
+        facingRight = !facingRight;
     }
 }
