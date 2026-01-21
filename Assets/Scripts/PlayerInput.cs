2ab2127ae25e916eddc52708a18ede5978893ab7
Carla Pallés Ros <carlaa.pr15@gmail.com>
2026-01-21 18:04:36 +0100

    Changed scene and assets names

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,34 +1,32 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerInput : MonoBehaviour
 {
-    [SerializeField]
-    float walkSpeed = 5.0f;
-    [SerializeField]
-    float jumpSpeed = 5.0f;
+    [SerializeField] float walkSpeed = 5.0f;
 
     Rigidbody2D rb2D;
+    [SerializeField] Animator animator;
 
     InputAction moveAction;
     Vector2 movement;
-
-    [SerializeField]
-    Animator animator;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
+
         moveAction = InputSystem.actions.FindAction("Move");
+        moveAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
         movement = moveAction.ReadValue<Vector2>();
-        animator.SetFloat("Speed", Mathf.Abs(movement.x));
+        animator.SetFloat("Walk", Mathf.Abs(movement.x));
     }
 
     void FixedUpdate()
