b63a7b216a5da81c5e4030fd28e94380d52710ad
Arnau Badenas <4bvives@gmail.com>
2026-01-28 13:58:37 +0100

    Remove powerup duration

--- Assets/Scripts/ScriptableObjectsScripts/PowerUpPickUp.cs
+++ Assets/Scripts/ScriptableObjectsScripts/PowerUpPickUp.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using UnityEngine;
 
 public class PowerUpPickUp : MonoBehaviour
@@ -8,18 +7,11 @@ public class PowerUpPickUp : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.TryGetComponent<PlayerJump>(out PlayerJump player))
+        if (collision.TryGetComponent<PlayerJump>(out _))
         {
-            // Coroutine to remove the effect without stopping the game, done to the player because the power up will be deactivated.
-            _ = player.StartCoroutine(ApplyAndRemove(collision.gameObject));
+            effect.Apply(collision.gameObject);
             gameObject.SetActive(false);
         }
     }
 
-    private IEnumerator ApplyAndRemove(GameObject target)
-    {
-        effect.Apply(target);
-        yield return new WaitForSeconds(effect.Duration);
-        effect.Remove(target);
-    }
 }
--- Assets/Scripts/ScriptableObjectsScripts/Powerup.cs
+++ Assets/Scripts/ScriptableObjectsScripts/Powerup.cs
@@ -2,8 +2,6 @@ using UnityEngine;
 
 public abstract class PowerUp : ScriptableObject
 {
-    public float Duration;
-
     public abstract void Apply(GameObject target);
     public abstract void Remove(GameObject target);
 }
